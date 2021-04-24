d52ef494357896802a24b646c8c8b8c56c5f42ef
Vikas Verma <vermavikas1002@gmail.com>
2021-04-25 01:17:09 +0530

    test cases added

--- src/test/java/project4/projecttest.java
+++ src/test/java/project4/projecttest.java
@@ -2,13 +2,41 @@ package project4;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class projecttest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void SUM() {
+		System.out.println("SUM");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void SUB() {
+		System.out.println("SUB");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void MULTI() {
+		System.out.println("MULTI");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+	@Test
+	public void DIV() {
+		System.out.println("DIV");
+		int a = 10;
+		int b = 20;
+		Assert.assertEquals(2, b/a);
 	}
 
 }
