8dd5a7a308cac03bd284a53deee499bc78f3c3e0
Vikas Verma <vermavikas1002@gmail.com>
2021-04-24 23:26:36 +0530

    initial commit

---
+++ src/main/java/project4/project4.java
@@ -0,0 +1,10 @@
+package project4;
+
+public class project4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is devops");
+	}
+
+}
---
+++ src/test/java/project4/projecttest.java
@@ -0,0 +1,14 @@
+package project4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class projecttest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
